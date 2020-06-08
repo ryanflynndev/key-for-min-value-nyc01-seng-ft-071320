cb4f70a196fecddc646f755a3e12f1760e29b9be
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-08 16:17:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,10 @@
 
 def key_for_min_value(name_hash)
   num = 100000000
+  if name_hash
   name_hash.each do |key, value|
-    if num < value 
-      
+    if num > value 
+      num = key
+    end
+     
 end
